29977f6c381c6080279d3935dc2db9ca5814d2f7
Momin Sarker <mdmsarker75@gmail.com>
2023-07-20 04:14:13 -0700

    LabWork1

---
+++ src/GreetingRunner.java
@@ -0,0 +1,8 @@
+public class GreetingRunner {
+    public static void main(String[] args) {
+        Greetings greetings = new Greetings();
+        greetings.sayHello();
+        greetings.saySagato();
+        greetings.sayBonjour();
+    }
+}
---
+++ src/Greetings.java
@@ -0,0 +1,11 @@
+public class Greetings {
+    public void sayHello(){
+        System.out.println("Hello");
+    }
+    public void saySagato(){
+        System.out.println("স্বাগত");
+    }
+    public void sayBonjour(){
+        System.out.println("Bonjour");
+    }
+}
